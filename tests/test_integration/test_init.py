15aa4175a897bd2191340e787f86accfe8fa4b86
Javed Habib <jh4official@gmail.com>
2024-08-01 20:42:57 +0530

    chore: reformat files using `make fl` beacause some formatting need to be changed to account for project name width

--- tests/test_integration/test_init.py
+++ tests/test_integration/test_init.py
@@ -8,7 +8,5 @@ import pytest
 @pytest.fixture
 def init_file_exists():
     """Check if the __init__.py file exists."""
-    path_to_init_file = (
-        Path(__file__).parents[2] / "tus_storagehandler" / "__init__.py"
-    )
+    path_to_init_file = Path(__file__).parents[2] / "tus_storagehandler" / "__init__.py"
     assert path_to_init_file.exists(), f"File {path_to_init_file} does not exist."
