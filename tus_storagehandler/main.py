906a4a8c09862afcb2ad8edf055f9f1ac72a524e
Javed Habib <jh4official@gmail.com>
2024-08-01 20:35:42 +0530

    chore(init): initialize project with https://github.com/elixir-cloud-aai/cookiecutter-python.git

---
+++ tus_storagehandler/main.py
@@ -0,0 +1,10 @@
+"""Entry point for tus_storagehandler."""
+
+
+def main():
+    """Main entry point for tus_storagehandler."""
+    print("Hello from tus_storagehandler!")
+
+
+if __name__ == "__main__":
+    main()
