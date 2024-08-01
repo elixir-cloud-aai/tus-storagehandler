551823ac277d8db361adaf23ac83d582493df5b0
Javed Habib <jh4official@gmail.com>
2024-08-01 21:05:43 +0530

    fix: locally fix https://github.com/elixir-cloud-aai/cookiecutter-python/issues/9 without updating .cruft.json

--- tests/test_integration/test_init.py
+++ tests/test_integration/test_init.py
@@ -2,11 +2,8 @@
 
 from pathlib import Path
 
-import pytest
 
-
-@pytest.fixture
-def init_file_exists():
+def test_init_file_exists():
     """Check if the __init__.py file exists."""
     path_to_init_file = Path(__file__).parents[2] / "tus_storagehandler" / "__init__.py"
     assert path_to_init_file.exists(), f"File {path_to_init_file} does not exist."
